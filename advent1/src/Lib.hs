1dd6cd62756cd53068573c2f49c2d1ef9cbb445a
Patrick Haugh <haughpj@gmail.com>
2019-12-04 20:17:01 -0500

    Day 1 2 compete

--- advent1/src/Lib.hs
+++ advent1/src/Lib.hs
@@ -9,10 +9,16 @@ doParse :: [Char] -> Int
 doParse ('+':cs) = read cs
 doParse cs = read cs
 
+findDup :: Eq a => [a] -> [a] -> a
+findDup _ [] = error "Could not find dupe"
+findDup seen (x:xs) = if elem x seen then x else findDup (x:seen) xs
+
 someFunc :: IO ()
 someFunc = do
     (dat:_) <- getArgs
     content <- readFile dat
     let words = filter ((/= 0) . length) (splitOn "\n" content)
     let ints = map doParse words
-    print $ sum ints     
+    let inf = cycle ints
+    let freqs = scanl (+) 0 inf
+    print $ findDup [] freqs
